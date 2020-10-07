c4c77da5a4e143b32500586defff27eae6d51274
Arjun Nair <nair.arjun47@yahoo.com>
2020-10-06 23:14:50 -0700

    Routes
    Add resoucres into the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get 'superhero/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :superhero do
+    resources :abilities, only: [:index]
+  end
+
+  resources :abilities, only: [:show, :update, :create, :destroy]
 end
